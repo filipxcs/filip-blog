db97bee6984d73c169b3fcfc3c7b1a6008f5026f
filipxcs <filippoupetros6@gmail.com>
2024-05-26 02:43:14 +0300

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@vueuse/nuxt", "nuxt-icon"],
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "nuxt-icon",
+    "@samk-dev/nuxt-vcalendar",
+    "@vee-validate/nuxt",
+    "@morev/vue-transitions/nuxt",
+    "@nuxtjs/strapi",
+    "@nuxtjs/seo"
+  ],
 
   tailwindcss: {
     exposeConfig: true,
@@ -16,16 +27,33 @@ export default defineNuxtConfig({
   },
 
   imports: {
-    imports: [
-      {
-        from: "tailwind-variants",
-        name: "tv",
-      },
-      {
-        from: "tailwind-variants",
-        name: "VariantProps",
-        type: true,
-      },
-    ],
+    imports: [{
+      from: "tailwind-variants",
+      name: "tv",
+    }, {
+      from: "tailwind-variants",
+      name: "VariantProps",
+      type: true,
+    }, {
+      from: "vue-sonner",
+      name: "toast",
+      as: "useSonner"
+    }],
   },
+
+  app: {
+    head: {
+      script: [{
+        src: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.9/pdfmake.min.js",
+        defer: true
+      }, {
+        src: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.9/vfs_fonts.min.js",
+        defer: true
+      }]
+    }
+  },
+
+  build: {
+    transpile: ["vue-sonner"]
+  }
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,107 @@
-<script setup lang="ts">
-  const route = useRoute();
-</script>
-
 <template>
-  <div>
-    <h1>Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
-      >Learn more about Nuxt Routing</a
-    >
-  </div>
+  <UiContainer class="py-16 lg:py-24">
+    <p class="mb-3 font-semibold text-primary">Our blog</p>
+    <h1 class="mb-4 text-4xl font-semibold lg:mb-6 lg:text-5xl">Resources and insights</h1>
+    <p class="text-lg text-muted-foreground lg:text-xl">
+      The latest industry news, interviews, technologies, and resources.
+    </p>
+
+    <div class="mb-10 mt-16 lg:hidden">
+      <NuxtLink :to="link">
+        <!-- eslint-disable-next-line vue/html-self-closing -->
+        <img
+          src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          :alt="alt"
+          class="mb-5 h-[240px] w-full rounded-lg object-cover shadow"
+        />
+      </NuxtLink>
+      <p v-if="headline" class="mb-2 text-sm font-semibold text-primary">{{ headline }}</p>
+      <NuxtLink :to="link">
+        <p class="mb-2 text-xl font-semibold lg:text-2xl">{{ title }}</p>
+      </NuxtLink>
+      <p v-if="description" class="mb-5 line-clamp-2 text-ellipsis text-muted-foreground">
+        {{ description }}
+      </p>
+      <div class="flex items-center">
+        <UiAvatar
+          v-if="userImage"
+          :src="userImage"
+          :alt="userName"
+          class="mr-3 rounded-full bg-background shadow ring-1 ring-ring/30"
+        />
+        <div>
+          <p v-if="userName" class="text-sm font-semibold">{{ userName }}</p>
+          <p v-if="date" class="text-sm text-muted-foreground">{{ date }}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Filter -->
+    <UiTabs class="mt-5" :default-value="categories[0]">
+      <UiTabsList :pill="false" class="relative h-auto w-full justify-start gap-3 overflow-x-auto">
+        <template v-for="c in categories" :key="c">
+          <UiTabsTrigger
+            class="p-2 focus-visible:ring-0 focus-visible:ring-offset-0"
+            :pill="false"
+            :value="c"
+            >{{ c }}</UiTabsTrigger
+          >
+        </template>
+        <UiTabsIndicator />
+      </UiTabsList>
+    </UiTabs>
+
+    <!-- Articles -->
+
+    <section class="mt-12 grid grid-cols-1 gap-x-8 gap-y-12 md:grid-cols-2 lg:mt-16 lg:grid-cols-3">
+      <template v-for="n in 9" :key="n">
+        <div class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+          <NuxtLink :to="link">
+            <!-- eslint-disable-next-line vue/html-self-closing -->
+            <img
+              v-if="image"
+              :src="image"
+              :alt="alt"
+              class="mb-5 h-[240px] w-full rounded-lg object-cover shadow"
+            />
+          </NuxtLink>
+          <p v-if="headline" class="mb-2 text-sm font-semibold text-primary">{{ headline }}</p>
+          <NuxtLink :to="link">
+            <p class="mb-2 text-xl font-semibold lg:text-2xl">{{ title }}</p>
+          </NuxtLink>
+          <p v-if="description" class="mb-5 line-clamp-2 text-ellipsis text-muted-foreground">
+            {{ description }}
+          </p>
+          <div class="flex items-center">
+            <UiAvatar
+              v-if="userImage"
+              :src="userImage"
+              :alt="userName"
+              class="mr-3 rounded-full bg-background shadow ring-1 ring-ring/30"
+            />
+            <div>
+              <p v-if="userName" class="text-sm font-semibold">{{ userName }}</p>
+              <p v-if="date" class="text-sm text-muted-foreground">{{ date }}</p>
+            </div>
+          </div>
+        </div>
+      </template>
+    </section>
+  </UiContainer>
 </template>
+
+<script lang="ts" setup>
+  const categories = ["Design", "Research", "Presentation", "Development", "Marketing", "Sales"];
+
+  const headline = "Design";
+  const title = "UX review presentations";
+  const image =
+    "https://images.unsplash.com/photo-1524169220946-12efd782aab4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const alt = "UX review presentations";
+  const description =
+    "How do you create compelling presentations that wow your colleagues and impress your managers?";
+  const date = "30 Jan 2024";
+  const userImage = "https://api.dicebear.com/7.x/lorelei/svg?flip=false";
+  const userName = "John Doe";
+  const link = "#";
+</script>
